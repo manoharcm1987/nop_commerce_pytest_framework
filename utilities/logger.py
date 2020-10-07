dc0c9e53ffe5d25ecc5052899bb2e9541f8e2bfa
Manohar CM <manoharcm1987@gmail.com>
2020-10-07 18:02:29 +0530

    Update logger.py

--- utilities/logger.py
+++ utilities/logger.py
@@ -7,7 +7,7 @@ class Logger:
     def get_logger():
 
         #path = "C://Old D drive//SeleniumWorkSpace//nop_commerce_pytest_framework//logs//nop_commerce.log"
-        path = os.path.join(pathlib.Path(__file__).parent.absolute(), "logs", "nop_commerce.log")
+        path = os.path.join(pathlib.Path(__file__).parent.parent.absolute(), "logs", "nop_commerce.log")
         log_format = "%(asctime)s:: %(levelname)s:  %(filename)s:%(lineno)d    %(message)s"
         date_format = '%m-%d-%Y %I:%M:%S %p'
         #logging.basicConfig(filename=path, format="%(asctime)s:: %(levelname)s:  %(filename)s:%(lineno)d    %(message)s",
